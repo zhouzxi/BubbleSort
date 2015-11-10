93e790b53c71c0e302c2a98e4ee65273040cb459
zhouzxi <zhouzxi@126.com>
2015-11-10 15:53:37 +0800

    Create BubbleSort.h

---
+++ BubbleSort.h
@@ -0,0 +1,21 @@
+/**********************************************************************
+* 版权所有 (C)2015, Zhou Zhaoxiong。
+*
+* 文件名称：BubbleSort.h
+* 文件标识：无
+* 内容摘要：冒泡排序演示程序
+* 其它说明：无
+* 当前版本：V1.0
+* 作    者：Zhou Zhaoxiong
+* 完成日期：20151009
+*
+**********************************************************************/
+#include <stdio.h>
+
+
+// 数据类型重定义
+typedef signed   int        INT32;
+typedef unsigned int        UINT32;
+
+// 函数声明
+void BubbleSort(INT32 *pszArray, UINT32 iArrayLen);
